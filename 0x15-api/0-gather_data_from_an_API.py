cb794f04c1210e79447c0f33956453d95815682f
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-10-23 16:37:30 +0300

    python script that prints information about the TODO list for a given id

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""for a given id return information about their TODO list"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+
+    res = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}')
+    tasks = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}/todos')
+    done_tasks = []
+    if res.status_code == 200:
+        data = res.json()
+        tasks = tasks.json()
+        all_tasks = len(tasks)
+        for task in tasks:
+            if task.get("completed") is True:
+                done_tasks.append(task.get("title"))
+        print(f'Employee {data.get("name")} is done'
+              f'with tasks({len(done_tasks)}/{all_tasks}):')
+        print('\n'.join('\t ' + task for task in done_tasks))
