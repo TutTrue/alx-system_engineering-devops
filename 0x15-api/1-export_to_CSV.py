5c151cc912d2e14f175f75c4c346b68f31df1336
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-10-23 17:28:12 +0300

    user variables instead of calling same function

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 """for a given id return information about their TODO list"""
 
-import requests
-import sys
 import csv
+import sys
+import requests
 
 
 if __name__ == '__main__':
 
+    user_id = sys.argv[1]
     res = requests.get(
-        f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}')
+        f'https://jsonplaceholder.typicode.com/users/{user_id}')
     tasks = requests.get(
-        f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}/todos')
+        f'https://jsonplaceholder.typicode.com/users/{user_id}/todos')
     if res.status_code == 200:
         data = res.json()
         tasks = tasks.json()
-        with open(f'{sys.argv[1]}.csv', 'w', newline='') as f:
+        user_name = data.get("name")
+        with open(f'{user_id}.csv', 'w', newline='') as f:
             writer = csv.writer(f, quoting=csv.QUOTE_ALL)
             for task in tasks:
-                writer.writerow([sys.argv[1], data.get("name"),
+                writer.writerow([user_id, user_name,
                                  task.get("completed"), task.get("title")])
